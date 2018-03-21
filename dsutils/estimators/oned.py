ec4e29486f030cc084a97db92a2ee146d2cd1c3c
cmardiros <carmen@mardiros.net>
2018-03-21 16:01:43 +0100

    Add distinct count function for 1-d arrays

---
+++ dsutils/estimators/oned.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+"""
+Simple estimators to apply to 1d arrays
+"""
+
+
+def distinct_count(data,
+                   notnull=True):
+    """
+    Return distinct count of some 1-d data.
+
+
+    Parameters:
+        notnull: bool, optional
+            If true, nulls are dropped before computing distinct count
+    """
+
+    # make a pd series as that can drop nulls in non-numeric arrays
+    if not isinstance(data, pd.Series):
+        data = pd.Series(data)
+
+    if notnull:
+        data = data[~data.isnull()]
+
+    return data.unique().shape[0]
